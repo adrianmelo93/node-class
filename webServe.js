a97cd60e61393f5879abd5189a489727033f7db3
Adrian Melo <amelo2@fvi.edu>
2017-04-26 20:43:20 -0400

    class nodejs

---
+++ webServe.js
@@ -0,0 +1,24 @@
+ // Miny web server to output "Thanks for calling" to the browser
+var http = require('http');
+const PORT = 8080;
+
+function process_request(req, res) {
+  var body = 'Thanks for calling!\n';
+  var content_length = body.length;
+  res.writeHead(200, {
+    'Content_Length': content_length,
+    'Content_Type': 'text/plain'
+  });
+
+  res.end(body);
+}
+
+var server = http.createServer(process_request);
+
+server.listen(PORT, () => {
+  console.log('listening on http://locahost: ' + PORT);
+});
+
+// server.listen(8080, function (){
+//   console.log('listening on http://locahost: 8080');
+// });
