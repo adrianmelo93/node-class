2638e2f7f88faaae75e79f8b131c4c09d1636f33
Adrian Melo <amelo2@fvi.edu>
2017-04-27 20:02:31 -0400

    ok

---
+++ express1/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+const PORT = 8080;
+
+app.get('/', function(req, res){
+  res.end('Hello Word');
+});
+
+app.listen(PORT, function(){
+  console.log(`listening on port ${PORT}`);
+});
